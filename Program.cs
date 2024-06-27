7bda0555966cc1872bfeaefbe4b3a129ca24ca34
User <User@WIN11DEV>
2024-06-26 20:07:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (OracleConnection cn = new OracleConnection(Environment.GetEnvironmentVariable("pimsdev_conn_str")))
+            {
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "WO_to_LIMS_Wrapper";
+                cmd.Parameters.Add("pnWOID", OracleDbType.Int16).Value = 300451;
+                cmd.Parameters.Add("psUser", OracleDbType.Char).Value = "BANDERSON";
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
